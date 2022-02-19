4e1d5932b46a6e405a37173dff396bfe83b194b0
Herry333 <98563053+Herry333@users.noreply.github.com>
2022-02-19 15:56:08 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,32 +5,39 @@ var userList=[
     },
 ];
 var a =true;
+var flag=false;
 while(a){
     var input=prompt('SignIn / Login/ Quit');
+    
     if(input==="signin"){
         userName=prompt("Enter UserName");
         password=prompt("enter Password");
-        userList.push({username:userName , passowrd:password});
+        userList.push({userName:userName , password:password});
         console.log(`UserName :${userName} -- Password: ${password}`);
-    }
-    
+        }
     else if (input==='login'){
         let userNameLogin=prompt("Enter UserName");
         let passwordLogin=prompt("Enter Password");
-        //userList.indexOf({username:userNameLogin});
-        for(let i=0;i<userList.length;i++){
-            if(userList[i].userName===userNameLogin && userList[i].password ===passwordLogin){
-            // console.log("welcome");
-            
+        for( element of userList){
+            if( element.userName===userNameLogin && element.password===passwordLogin){
+            flag=true;
+            console.log(`welcome ${element.userName}`);
+            let option=prompt(" What u want to do 1/2/3 ");
+            console.log(`Press 1 to add product`);
+            console.log(`Press 2 to add product`);
+            console.log(`Press 3 to add product`);
+            if(option==1){}
+            if(option==2){}
+            if(option==3){}
             }
-            else{console.log("invalid Email or Password");}
-        } 
+        }
+        if (flag==false){
+            console.log("invalid Username/ PAssword");
+        }
     }
     else if (input==='quit')
     {
         a=false;
     }
-    // var input=prompt('SignIn / Login/ Quit');
-
 }
 console.log("quit application");
