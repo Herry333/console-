76964ccb9e34024ba0b80d11f20f6d38638837f8
Herry333 <98563053+Herry333@users.noreply.github.com>
2022-02-22 16:28:19 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-console.log('asdasdasdasd');
+let arr=[]
+let sum=0;
+// console.log('asdasdasdasd');
 var userList=[
     {
         userName:"haider",
@@ -11,24 +13,28 @@ var flag=false;
 while(a){
     var input=prompt('SignIn / Login/ Quit');
     
-    if (input==="signin"){
+    if (input==="signin")
+    {
         userName=prompt("Enter UserName");
         password=prompt("enter Password");
         userList.push({userName:userName , password:password});
         console.log(`UserName :${userName} -- Password: ${password}`);
-        }
-    else if (input==='login'){
+    }
+    else if (input==='login'){ 
         let userNameLogin=prompt("Enter UserName");
         let passwordLogin=prompt("Enter Password");
+        z=true;
+
         for( element of userList)
         {
-            if( element.userName===userNameLogin && element.password===passwordLogin){
+            if( element.userName===userNameLogin && element.password===passwordLogin)
+            {
                 flag=true;
                 console.log(`welcome ${element.userName}`);
-                while( z=true){
+                while(z)
+                {
                 console.log(`Press 1 to sell product`);
                 console.log(`Press 2 to buy products`);
-                // console.log(`Press 4 to sell products on discount`);
                 console.log(`Press 5 to see Inventory`);
                 let option=parseInt(prompt(" What u want to do 1/2/5 "));
                 if(option==1)
@@ -36,23 +42,24 @@ while(a){
                     let name=prompt(`Enter Product Name`);
                     let id=parseInt(prompt(`Enter Product ID`));
                     let cost=parseInt(prompt(`Enter Cost of product`));
+                    let discount=parseInt(prompt('Discount on the product ( in %)'));
                     let quantity=parseInt(prompt(`How much  ${name} you want to add`));
-                    products.push({pName:name, pId:id, pCost:cost ,pQuantity:quantity});
-                    console.log(`${products.pName}item added in the list`);
+                    products.push({pName:name, pId:id, pCost:cost ,pQuantity:quantity, pDiscount:discount});
+                    console.log(`${products.pName} item added in the list`);
                 }
                 if(option==5)
                 {
-                    for(let product of products){
-                    console.log(`ID:${product.pId} NAME:${product.pName} Price:${product.pCost} and  Quantity :${product.pQuantity} `);
+                    for(let product of products)
+                    {
+                    console.log(`ID:${product.pId} NAME:${product.pName} Price:${product.pCost} and  Quantity :${product.pQuantity} Discount:${product.pDiscount} `);
                     }
                 }
                 if(option==2)
                 {
-                    let sum=0;
                     console.log(`---LIST---`)
                     for(let product of products)
                     {
-                        console.log(`ID:${product.pId} NAME:${product.pName} Price:${product.pCost} and  Quantity :${product.pQuantity} `);
+                        console.log(`ID:${product.pId} NAME:${product.pName} Price:${product.pCost} and  Quantity :${product.pQuantity} Discount:${product.pDiscount} `);
                     }
                     let itemToBuy=parseInt(prompt(`Enter the ID of product you want to buy`));
                     for(z of products)
@@ -63,29 +70,34 @@ while(a){
                             {
                                 buyQuantity=parseInt(prompt(`how much you need `));
                                 z.pQuantity = z.pQuantity-buyQuantity;
+                                arr.push({Name:z.pName, Qty:buyQuantity , cost:sum});
                                 console.log(`---------------------your cart-------------`);
-                                sum=z.pCost * buyQuantity;
+                                let dCost=z.pCost*z.pDiscount/100;
+                                sum =sum + (dCost * buyQuantity);
+                                console.log(arr);
                                 console.log(`Name:${z.pName} Qty:${buyQuantity}Total :Rs ${sum}`);
                                 console.log(`thanks for buying `);
+                                
+                                
                             }else
                             {
                                 console.log(`Out of stock`);
                             }
                         }
-                        else
-                        {
-                            console.log(`sorry these is no such product in the store`);
-                        }         
+                     
                     }
-                }
-                let m=prompt("logout ?");
-                if (m=='yes'){
-                    z=false; 
-                }
+                
+                    if (z.pId!=itemToBuy){
+                        console.log(`sorry these is no such product in the store`);
+                    }
+                    }
+                    let m=prompt("logout ?");
+                    if (m==='yes'){
+                        z=false; 
+                    }
                 }
             }
         }
-
         if (flag==false)
         {
             console.log("invalid Username/ PAssword");
